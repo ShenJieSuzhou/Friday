c8391e5ae6d4b6f505c75479aca49fd66d969914
ShenJieSuzhou <745689122@qq.com>
2016-11-22 17:20:26 +0800

    测试 添加block回调

--- TestFriday/TestFriday/YYWebImageManager.h
+++ TestFriday/TestFriday/YYWebImageManager.h
@@ -5,8 +5,11 @@
 //  Created by silicon on 16/11/16.
 //  Copyright © 2016年 com.snailgames. All rights reserved.
 //
-
+#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
+
+typedef UIImage * _Nullable (^YYWebImageTransformBlock)(UIImage *image, NSURL *url);
+
 
 @interface YYWebImageManager : NSObject
 
